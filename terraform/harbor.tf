a5340d91d7600b07ac49f82195220e1e8719fba4
Robin <robin.thiriet@ynov.com>
2026-06-22 14:23:30 +0200

    add harbor helm

---
+++ terraform/harbor.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_namespace_v1" "harbor" {
+  metadata {
+    name = "harbor"
+  }
+}
+
+resource "helm_release" "harbor" {
+  name      = "harbor"
+  namespace = "harbor"
+
+  repository = "https://helm.goharbor.io"
+  chart      = "harbor"
+
+  values = [
+    file("${path.module}/harbor-values.yaml")
+  ]
+
+  wait    = true
+  timeout = 900
+}
